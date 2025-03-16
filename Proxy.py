9d7871dd12618961878483be8dac64deae64bbeb
Tung Hoang <hoangthanhtung285@gmail.com>
2025-03-16 23:09:05 +1030

    fill in request and request header

--- Proxy.py
+++ Proxy.py
@@ -149,6 +149,8 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
+      originServerRequest = f'{method} {resource} HTTP/1.1'
+      originServerRequestHeader = f'Host: {hostname}'
       # ~~~~ END CODE INSERT ~~~~
 
       # Construct the request to send to the origin server
