311d5ed742f95ff71d60058072c718e50e475e4a
Tung Hoang <hoangthanhtung285@gmail.com>
2025-03-16 23:24:30 +1030

    made changes to request line, add code to receive response from server and send it to client. save response to file system

--- Proxy.py
+++ Proxy.py
@@ -149,7 +149,7 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
-      originServerRequest = f'{method} {resource} HTTP/1.1'
+      originServerRequest = f'{method} {resource} {version}'
       originServerRequestHeader = f'Host: {hostname}'
       # ~~~~ END CODE INSERT ~~~~
 
@@ -171,10 +171,18 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
+      response = b''
+      while True:
+        data = originServerSocket.recv(BUFFER_SIZE)
+        if len(data) > 0:
+          response += data
+        else:
+          break
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+      clientSocket.send(response)
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
@@ -186,6 +194,7 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+      cacheFile.write(response)
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
