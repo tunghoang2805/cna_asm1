e0a926ebf30265c18b08eb1485baf48fca322680
Tung Hoang <hoangthanhtung285@gmail.com>
2025-03-16 22:58:59 +1030

    initialize and connect origin server socket

--- Proxy.py
+++ Proxy.py
@@ -129,6 +129,7 @@ while True:
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
+    originServerSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
@@ -137,6 +138,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+      originServerSocket.connect((address,81))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
